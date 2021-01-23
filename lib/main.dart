4b94a0e437252e3d701065e66a7a5055d372a9dc
DhrumilGandhi <dhrumilgandhi540@gmail.com>
2021-01-23 14:32:18 +0530

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "First App",
-      home: Scaffold(
-        appBar: AppBar(title: Text("My First APP")),
-        body: Material(
-          color: Colors.blueAccent,
-          child: Center(
-            child: Text(
-              "Hello This is My First Mobile APP",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 25),
-            ),
-          ),
-        ),
-      )));
+  runApp(HomeScreen());
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "First App",
+        home: Scaffold(
+          appBar: AppBar(title: Text("My First APP")),
+          body: Material(
+              child: Container(
+            margin: EdgeInsets.all(10.0),
+            child: Table(border: TableBorder.all(), children: [
+              TableRow(children: [
+                Text(
+                  "First Name",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Last Name",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                )
+              ]),
+              TableRow(children: [
+                Text(
+                  "Dhrumil",
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Gandhi",
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.center,
+                )
+              ]),
+              TableRow(children: [
+                Text(
+                  "Pooja",
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Gandhi",
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.center,
+                )
+              ])
+            ]),
+          )),
+        ));
+  }
 }
